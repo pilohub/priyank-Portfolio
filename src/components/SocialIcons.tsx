6cc5a4b1748753b8f23bb55393c6e56a47014067
Priyank Vadadoriya <priyankvaddoriya10@gmail.com>
2026-05-14 16:50:16 +0530

    small update

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="Resume.pdf">
+      <a className="resume-button" href="priyank.pdf">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
